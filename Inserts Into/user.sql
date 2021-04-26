a0bf6cd02734a4c42d6961f3524a171412bdefa3
ivanhitosm <73246987+ivanhitosm@users.noreply.github.com>
2021-04-26 22:37:04 +0200

    Add files via upload

---
+++ Inserts Into/user.sql
@@ -0,0 +1,18 @@
+create table user (
+	username VARCHAR(50),
+	hash VARCHAR(50),
+	email VARCHAR(50),
+	created_at DATE,
+	updated_at DATE,
+	email_verificate_at DATE
+);
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('ncaldecott0', 'd9d5b240b11408e1049826b5b9ca8b87', 'dmarqyes0@cnet.com', '6/7/2020', '3/20/2021', '10/15/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('htrodden1', 'f256991e213d596bed538f6a58ce7264', 'alamplough1@google.com.hk', '10/8/2020', '8/21/2020', '7/9/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('sphil2', 'a4dac0af7b732a7691d3be5c8478752a', 'umullin2@instagram.com', '5/18/2020', '2/23/2021', '6/17/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('tdugan3', '72e81493b7449707a9eab3afe1e3229c', 'efall3@tuttocitta.it', '1/20/2021', '11/24/2020', '7/11/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('alomond4', 'fa3f270f68c7d6b6fa9f755efc4520ac', 'lirwin4@creativecommons.org', '12/6/2020', '11/25/2020', '12/13/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('tcron5', '3d2a1e8abbbe93386b6bbcad93653d77', 'rbusch5@ft.com', '2/1/2021', '9/4/2020', '11/16/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('pkernoghan6', '432de1e80974d9e33c395bc4f52696a6', 'bhawkyens6@cyberchimps.com', '4/23/2021', '4/8/2021', '10/20/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('vdunthorn7', 'f3509d2d80fe9b6c6cc5e4b014248132', 'tguitonneau7@constantcontact.com', '2/19/2021', '9/27/2020', '9/26/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('twhisker8', '2b5e68408e7429364a3d7808c6317ddc', 'cbaggaley8@purevolume.com', '12/14/2020', '2/16/2021', '12/7/2020');
+insert into user (username, hash, email, created_at, updated_at, email_verificate_at) values ('jfriar9', 'e384b0a557e71656e18868796692e4bc', 'bhagwood9@51.la', '1/15/2021', '9/7/2020', '10/20/2020');
